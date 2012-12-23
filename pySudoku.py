2a8e6be4fc842d641aa69cb1184e27f7c74dbdee
Paul Moon <moon.haein@gmail.com>
2012-12-23 09:56:27 -0800

    Implemented the initial basic logic part of the algorithm.

--- pySudoku.py
+++ pySudoku.py
@@ -2,21 +2,70 @@
 # December 2012
 # pySudoku.py
 
+# First try to solve by filling in the cells with only one possibility.
+# If it cannot go any further, use a backtracking DFS (depth-first search)
+# algorithm to try the possible solutions.
+
+# The algorithm assumes that empty cells are denoted with a 0.
+
 import fileinput
+
+def print_sudoku(s):
+    for row in s:
+        for num in row:
+            print num, 
+        print
+    print
+
+def initial_try(s):
+    stuck = False
+    while not stuck:
+        stuck = True
+
+        # Iterate through the Sudoku puzzle
+        for row in range(9):
+            for col in range(9):
+                used = [0]*10
+                used[0] = 1
+                block_row = row / 3
+                block_col = col / 3
+
+                # Row and Column
+                for m in range(9):
+                    used[s[m][col]] = 1;
+                    used[s[row][m]] = 1;
+
+                # Square
+                for m in range(3):
+                    for n in range(3):
+                        used[s[m + block_row*3][n + block_col*3]] = 1
+
+                # More than one possibility
+                if (used.count(0) != 1):
+                    continue
+
+                for m in range(1, 10):
+                    if (s[row][col] == 0 and used[m] == 0):
+                        s[row][col] = m
+                        stuck = False
+                        break
 
 def solve(s):
     pass
 
 def main():
     s = []
-    count = 0
 
     for line in fileinput.input():
         # Assumes Sudokus are separated by an empty line
+        # There must be an empty line at the end of file
         if line not in ['\n', '\r\n']:
-             s.append(line)
+            l = [int(num) for num in line.split() if num.isdigit()]
+            s.append(l)
         else:
-            solve(s)
+            print_sudoku(s)
+            initial_try(s)
+            print_sudoku(s)
             s = []
 
 if __name__ == "__main__":
